5f0c79ac82a4cc99539f0b5f5a904c8ee9df4835
jagruti181 <patiljagruti181@gmail.com>
2015-02-05 12:59:10 +0530

    cache

--- application/views/json.php
+++ application/views/json.php
@@ -1,7 +1,4 @@
 <?php 
-   header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");//Dont cache
-   header("Pragma: no-cache");//Dont cache
-   header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");//Make sure it expired in the past (this can be overkill)
 $http_origin = $_SERVER['HTTP_ORIGIN'];
 header('Content-type: application/javascript');
 header('Access-Control-Allow-Headers: Content-Type');
