3d4c7adb970e8d757afda309f474222c171e8482
Avinash <avinash@wohlig.com>
2015-02-05 12:19:03 +0530

    cache

--- application/views/json.php
+++ application/views/json.php
@@ -1,23 +1,13 @@
 <?php 
-//header('Content-type: application/javascript');
-//header("Access-Control-Allow-Origin: *");
-
-
-
-
-
+   header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");//Dont cache
+   header("Pragma: no-cache");//Dont cache
+   header("Expires: Thu, 19 Nov 1981 08:52:00 GMT");//Make sure it expired in the past (this can be overkill)
 $http_origin = $_SERVER['HTTP_ORIGIN'];
 header('Content-type: application/javascript');
 header('Access-Control-Allow-Headers: Content-Type');
 header('Access-Control-Allow-Credentials: true');
 header("Access-Control-Allow-Origin: $http_origin");
 header('Access-Control-Max-Age: 86400');
-
-
-
-
-
-
 
 echo json_encode($message);
 ?>
